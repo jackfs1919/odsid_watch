829cbd85750b3352bb61fa644073c57c357ab80f
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2025-07-02 16:51:27 +0300

    added worked days count

--- obsid_watch.py
+++ obsid_watch.py
@@ -28,7 +28,7 @@ def ensure_directory_exists(path):
         os.makedirs(path)
 
 
-def find_changed_files(root_folder):
+def find_changed_files(root_folder, days):
     changes = []  # Список для хранения изменений
 
     for root, dirs, files in os.walk(root_folder):
@@ -41,7 +41,7 @@ def find_changed_files(root_folder):
             file_path = os.path.join(root, f)
             # Здесь вы можете добавить свою логику для проверки изменений файла
             # Например, проверка по времени последнего изменения
-            if os.path.getmtime(file_path) > time.time() - 86400 * d:  # файлы измененные за последний день
+            if os.path.getmtime(file_path) > time.time() - 86400 * days:  # файлы измененные за последний день
                 changes.append(file_path)
                 print(f'Измененный файл: {os.path.relpath(file_path, root_folder)}')
 
@@ -61,12 +61,12 @@ def unzip_files(zip_file_name, output_folder):
     print("Файлы распакованы")
 
 
-def main(action, root_folder, output_zip_path):
+def main(action, root_folder, output_zip_path, days):
     output_folder = root_folder
     ensure_directory_exists(output_zip_path)
 
     if action == 'p':
-        changed_files = find_changed_files(root_folder)
+        changed_files = find_changed_files(root_folder, days)
         if changed_files:
             zip_file_name = os.path.join(output_zip_path, 'changed_files.zip')
             zip_changed_files(changed_files, zip_file_name)
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Упаковать или распаковать измененные файлы.')
     parser.add_argument('action', choices=['p', 'u'], help='p - упаковать изменившиеся файлы, u - распаковать архив')
     parser.add_argument('--path', help='Пользовательский путь к папке', default=None)
-    parser.add_argument('--d', help='Количество дней для отслеживания изменений', default=1)
+    parser.add_argument('--days', type=int, help='Количество дней для отслеживания изменений', default=1)
 
     args = parser.parse_args()
 
@@ -96,13 +96,13 @@ if __name__ == "__main__":
     computer_name = os.getenv('COMPUTERNAME')
     output_zip_path = "o:\\install\\obsid"
     paths = COMPUTER_PATHS.get(computer_name)
+    days = args.days if args.days else 1
     if paths:
         root_folder = args.path if args.path else paths['input_folder']
-        d = args.d if args.d else 1
     else:
         print(f'Нет настроенных путей для компьютера: {computer_name}')
         exit(1)
 
-    main(args.action, root_folder, output_zip_path)
+    main(args.action, root_folder, output_zip_path, days)
     print(f'время исполнения {round((time.perf_counter() - start_script), 2)} сек.')
     time.sleep(5)
