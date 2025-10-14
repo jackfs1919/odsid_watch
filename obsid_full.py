8ab2af69a78d6bf2728e6e30806a6ae31ba55c3f
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2025-10-14 14:43:07 +0300

    refact

--- obsid_full.py
+++ obsid_full.py
@@ -26,39 +26,46 @@ computer_name = os.getenv('COMPUTERNAME')
 paths = COMPUTER_PATHS.get(computer_name)
 
 
+def five_sec_exit(s=1):
+    time.sleep(5)
+    exit(s)
+
+
 def ensure_zipfile_exists(zip_path):
     global zip_file
     try:
         zip_file = list(pathlib.Path(zip_path).glob('*.zip'))[0].name
     except Exception as e:
         logger.error(colored(f'ZIP-файл не найден', 'red'))
-        exit()
+        five_sec_exit()
 
 
 def ensure_directory_exists(path):
     if not os.path.exists(path):
         logger.error(colored('Каталог архива недоступен', "red"))
-        
+        five_sec_exit()
+
 
 def unzip_files(zip_file_name, output_folder):
     with zipfile.ZipFile(zip_file_name, 'r') as zipf:
         zipf.extractall(output_folder)
     logger.info(colored("Файлы распакованы", "green"))
 
-    
+
 def clear_directory(path):
     if not os.path.isdir(path):
         raise ValueError(colored(f"Указанный путь не является директорией: {path}", "red"))
-    
     try:
         shutil.rmtree(path)
     except Exception as e:
-            logger.error(colored(f"Не удалось удалить {path}: {e}"), "red")
+        logger.error(colored(f"Не удалось удалить {path}: {e}"), "red")
+        five_sec_exit()
     try:
         os.makedirs(path, exist_ok=True)
     except Exception as e:
-            logger.error(colored(f"Не удалось создать {path}: {e}"), "red")
-    
+        logger.error(colored(f"Не удалось создать {path}: {e}"), "red")
+        five_sec_exit()
+
 
 @snoop
 def main(root_folder, output_zip_path):
@@ -76,6 +83,8 @@ def main(root_folder, output_zip_path):
         logger.info(colored('Файл архива удален', 'green'))
     else:
         logger.error(colored(f'ZIP-файл не найден: {zip_file_name}', 'red'))
+        five_sec_exit()
+        
 
 
 
@@ -84,8 +93,7 @@ if __name__ == "__main__":
         root_folder = paths['income_folder']
     else:
         logger.error(colored(f'Нет настроенных путей для компьютера: {computer_name}', "red"))
-        exit(1)
+        five_sec_exit()
     main(root_folder, ZIP_PATH)
     logger.info(f'время исполнения {colored(str(round((time.perf_counter() - start_script), 2)), "green")} сек.')
-    time.sleep(5)
-
+    five_sec_exit(0)
